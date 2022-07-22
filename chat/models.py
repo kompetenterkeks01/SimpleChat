0680744d4cc413202fb284dafc63dc7fd4f07eaa
Leinadix <daniel.s.hajek@gmail.com>
2022-07-22 20:00:09 +0200

    View extension

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    user_name = models.CharField(max_length=20)
+    msg = models.CharField(max_length=280)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+
+        return f"{str(self.user_name)} [{str(self.pub_date).split(' ')[1].split('.')[0]}]: {str(self.msg)}"
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
+from chat.models import Message
 
 def index(request):
-    return HttpResponse("Chathtml hier!")
+    allMsg = Message.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('index.html')
+    context = {
+        'allMsg': allMsg,
+    }
+    return HttpResponse(template.render(context, request))
